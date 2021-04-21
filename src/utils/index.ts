89007dc249d95b0cc6263002fc74bcadbdc3341a
yc <404147228@qq.com>
2021-04-21 18:27:10 +0800

    feat: ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,10 +2,10 @@
  * @Description:
  * @Author: YanCheng
  * @Date: 2021-04-21 16:30:07
- * @LastEditTime: 2021-04-21 16:45:30
+ * @LastEditTime: 2021-04-21 18:23:06
  */
-export const isFalsy = (value) => (value === 0 ? false : !value);
-export const cleanObject = (object) => {
+export const isFalsy = (value: any) => (value === 0 ? false : !value);
+export const cleanObject = <T>(object: T) => {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
     const value = object[key];
