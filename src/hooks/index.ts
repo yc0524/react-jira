bff0d9b172c827d4190a66e944369c96e387987d
yc <404147228@qq.com>
2021-04-22 10:45:57 +0800

    feat: 泛型

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,11 +2,11 @@
  * @Description:
  * @Author: YanCheng
  * @Date: 2021-04-21 16:52:17
- * @LastEditTime: 2021-04-22 09:25:25
+ * @LastEditTime: 2021-04-22 10:37:54
  */
 import { useEffect, useState } from "react";
 
-export const useDebounce = (value: any, delay?: number) => {
+export const useDebounce = <T>(value: T, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
@@ -15,4 +15,20 @@ export const useDebounce = (value: any, delay?: number) => {
   }, [delay, value]);
 
   return debouncedValue;
+};
+
+export const useArray = <T>(initialArray: T[]) => {
+  const [value, setValue] = useState(initialArray);
+
+  return {
+    value,
+    setValue,
+    add: (item: T) => setValue([...value, item]),
+    removeIndex: (index: number) => {
+      const copy = [...value];
+      copy.splice(index, 1);
+      setValue(copy);
+    },
+    clear: () => setValue([]),
+  };
 };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: YanCheng
  * @Date: 2021-04-21 16:30:07
- * @LastEditTime: 2021-04-22 09:21:17
+ * @LastEditTime: 2021-04-22 10:17:09
  */
 export const isFalsy = (value: any) => (value === 0 ? false : !value);
 export const cleanObject = <T>(object: T) => {
