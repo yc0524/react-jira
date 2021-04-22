3e3377c3fde07a0d1d4ef18f7d269105ec52d90f
yc <404147228@qq.com>
2021-04-22 14:25:30 +0800

    feat: jira-dev-tool

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
+/*
+ * @Description:
+ * @Author: YanCheng
+ * @Date: 2021-04-21 14:21:38
+ * @LastEditTime: 2021-04-22 14:17:53
+ */
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { loadDevTools } from "jira-dev-tool";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+loadDevTools(() =>
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
 );
 
 // If you want to start measuring performance in your app, pass a function
